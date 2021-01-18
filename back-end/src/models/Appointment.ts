cdbe5563450aba2c60fa814325c882b3b465e3a9
Rickson <cyberickson@gmail.com>
2021-01-18 19:55:03 -0300

    second commit

--- back-end/src/models/Appointment.ts
+++ back-end/src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
-    id: string;
-    
-    provider: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    date: Date;
+  @Column()
+  provider: string;
 
-    constructor({provider, date}: Omit<Appointment, 'id'>) {
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
+  @Column('timestamp with time zone')
+  date: Date;
 }
 
 export default Appointment;
